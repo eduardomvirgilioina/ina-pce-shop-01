69c3e1cbcd0511377de5ec79ab47be2f9fa621e0
EduardoMVirgilio <eduardomiguelvirgilio@gmail.com>
2025-11-19 21:47:41 -0300

    Flujo completo de Usuarios

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -1,9 +1,75 @@
-export function access(req, res) {
-  res.send("Acceso de usuario");
+import bcrypt from "bcryptjs";
+import { PrismaClient } from "../generated/prisma/client.js";
+const prisma = new PrismaClient();
+export async function access(req, res) {
+  const data = req.body;
+  if (!data.email || !data.clave) {
+    return res
+      .status(400)
+      .send({ msg: "Faltan datos obligatorios", error: true });
+  }
+  const user = await prisma.usuarios.findUnique({
+    where: { email: data.email },
+  });
+  if (!user) {
+    return res.status(404).send({ msg: "Usuario no encontrado", error: true });
+  }
+  const isPasswordValid = await bcrypt.compare(data.clave, user.clave);
+  if (!isPasswordValid) {
+    return res.status(401).send({ msg: "Clave incorrecta", error: true });
+  }
+  return res.status(200).send({ data: user });
 }
-export function profile(req, res) {
-  res.send("Perfil de usuario");
+export async function profile(req, res) {
+  const id = req.params.id;
+  const user = await prisma.usuarios.findUnique({
+    where: { id: Number(id) },
+    include: {
+      ordenes: {
+        include: {
+          items: true,
+        },
+      },
+    },
+  });
+  if (!user) {
+    return res.status(404).send({ msg: "Usuario no encontrado", error: true });
+  }
+  return res.status(200).send({ data: user });
 }
-export function save(req, res) {
-  res.send("Registro de usuario");
+export async function save(req, res) {
+  const data = req.body;
+  if (!data.nombre || !data.email || !data.clave) {
+    return res
+      .status(400)
+      .send({ msg: "Faltan datos obligatorios", error: true });
+  }
+  const regxEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  if (!regxEmail.test(data.email)) {
+    return res.status(400).send({ msg: "Email no válido", error: true });
+  }
+  // Validar que la clave tenga al menos una letra mayúscula, una minúscula , un número y un carácter especial
+  const regexClave =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+  if (!regexClave.test(data.clave)) {
+    return res.status(400).send({ msg: "Clave no válida", error: true });
+  }
+  // Validar si el email ya existe
+  const existingUser = await prisma.usuarios.findUnique({
+    where: { email: data.email },
+  });
+  if (existingUser) {
+    return res
+      .status(400)
+      .send({ msg: "El email ya está registrado", error: true });
+  }
+  const newUser = await prisma.usuarios.create({
+    data: {
+      nombre: data.nombre,
+      email: data.email,
+      clave: await bcrypt.hash(data.clave, 10),
+      es_admin: data.email.includes("@admin.com") ? true : false,
+    },
+  });
+  return res.status(200).send({ data: newUser });
 }
--- src/middlewares/admin.js
+++ src/middlewares/admin.js
@@ -1,6 +1,16 @@
 // Verifica si el usuario tiene rol de administrador
-export function isAdmin(req, res, next) {
-  console.log(req.headers["key"]);
+import { PrismaClient } from "../generated/prisma/client.js";
 
+export async function isAdmin(req, res, next) {
+  const auth = req.headers["Authorization"] || req.headers["authorization"];
+  if (!auth) return res.status(401).send({ error: "No autorizado" });
+  const prisma = new PrismaClient();
+  const user = await prisma.usuarios.findUnique({
+    where: { id: Number(auth) },
+  });
+  if (!user) return res.status(401).send({ error: "Usuario no encontrado" });
+  if (!user.es_admin) {
+    return res.status(403).send({ error: "Acceso denegado" });
+  }
   return next();
 }
